eb5d1760a8b3b02cc22543d64c584696b7d39772
IloveUhiuhiu <dangphuclong2019@gmail.com>
2024-04-11 09:35:56 +0700

    long day

---
+++ WebPBL3/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPBL3.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebPBL3/Controllers/CarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPBL3.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
