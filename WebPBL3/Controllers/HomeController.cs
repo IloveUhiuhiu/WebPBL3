efbdeea06bbfa36f9bdd0addb28649d5a1e68964
IloveUhiuhiu <dangphuclong2019@gmail.com>
2024-04-12 14:20:45 +0700

    along

--- WebPBL3/Controllers/HomeController.cs
+++ WebPBL3/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebPBL3.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View(); 
+        }
+
         public IActionResult Privacy()
         {
             return View();
