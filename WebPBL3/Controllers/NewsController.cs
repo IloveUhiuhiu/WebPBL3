a7f5ec6c612f5887a0551a6507f69eef5c07fc1c
hung55555hung <hung55555hung@gmail.com>
2024-04-20 00:49:19 +0700

    f

--- WebPBL3/Controllers/NewsController.cs
+++ WebPBL3/Controllers/NewsController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebPBL3.Models;
 
 namespace WebPBL3.Controllers
 {
     public class NewsController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DanhSachTinTuc()
+        {
+            return View(model: StaticNews.AllNews);
+        }
+        public IActionResult ThemTinTuc()
         {
             return View();
         }
