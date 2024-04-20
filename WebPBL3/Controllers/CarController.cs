a3c34cf941fbce3429926a137c52267fbb150418
IloveUhiuhiu <dangphuclong2019@gmail.com>
2024-04-20 10:50:59 +0700

    long

--- WebPBL3/Controllers/CarController.cs
+++ WebPBL3/Controllers/CarController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TextTemplating;
+using System.Drawing;
 using WebPBL3.Models;
 
 namespace WebPBL3.Controllers
@@ -20,14 +23,41 @@ namespace WebPBL3.Controllers
             return View();
         }
         public IActionResult CarListTable()
-        {   
-            List<Car> cars = _db.Cars.ToList();
-            
-            return View(cars);
+        {
+            List<CarDto> _cars = _db.Cars
+                .Include(c => c.Make)
+                .Select(c => new CarDto
+                {
+                    CarID = c.CarID,
+                    CarName = c.CarName,
+                    Price = c.Price,
+                    Seat = c.Seat,
+                    Origin = c.Origin,
+                    Dimension = c.Dimension,
+                    Capacity = c.Capacity,
+                    Topspeed = c.Topspeed,
+                    Color = c.Color,
+                    Photo = c.Photo,
+                    Year = c.Year,
+                    Engine = c.Engine,
+                    Quantity = c.Quantity,
+                    Description = c.Description,
+                    FuelConsumption = c.FuelConsumption,
+                    MakeName = c.Make.MakeName
+                })
+                .ToList();
+            return View(_cars);
         }
+        // [GET]
         public IActionResult Create()
         {
             return View();
+        }
+        // [POST]
+        [HttpPost]
+        public IActionResult Create(CarDto car)
+        {
+            return View(car);
         }
         public IActionResult Update()
         {
