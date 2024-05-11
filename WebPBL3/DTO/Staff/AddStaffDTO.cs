7200817318af6754fa2737f7cd313cdbf637b520
Caodiep <162148832+Caodiep@users.noreply.github.com>
2024-05-11 13:18:13 +0700

    diep finish staff

--- WebPBL3/DTO/Staff/AddStaffDTO.cs
+++ WebPBL3/DTO/Staff/AddStaffDTO.cs
@@ -1,27 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace WebPBL3.DTO.Staff
 {
-    public class AddStaffDTO
+    using System.Web;
+    public partial class AddStaffDTO
     {
         [Key]
-
         public string? StaffID { get; set; }
-        public string? FullName { get; set; }
-        public string? PhoneNumber { get; set; }
-        public string? IdentityCard { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string IdentityCard { get; set; }
         public bool? Gender { get; set; }
         public DateTime? BirthDate { get; set; }
         public string? Address { get; set; }
-        public string? Photo { get; set; }
         public string Position { get; set; }
         public int Salary { get; set; }
-        public string WardName { get; set; }
-        public string DistrictName { get; set; }
-        public string ProvinceName { get; set; }
+        public string? WardName { get; set; }
+        public string? DistrictName { get; set; }
+        public string? ProvinceName { get; set; }
         public string Email { get; set; }
         public string? Password { get; set; }
         public bool Status { get; set; }
-
+        [NotMapped]
+        public IFormFile? Photo { get; set; }
     }
 }
